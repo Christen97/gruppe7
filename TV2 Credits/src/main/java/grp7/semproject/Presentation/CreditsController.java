1d476637d674e01658847e3a4e01cd6df46a8b09
Mads Munch <madsmunch1998@gmail.com>
2020-04-09 17:33:51 +0200

    Added listviews and remove logic

--- TV2 Credits/src/main/java/grp7/semproject/Presentation/CreditsController.java
+++ TV2 Credits/src/main/java/grp7/semproject/Presentation/CreditsController.java
@@ -12,8 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 
 
 public class CreditsController {
@@ -21,6 +20,19 @@ public class CreditsController {
     public ListView castLV;
     public ListView audioLV;
     public ListView visualLV;
+    public Button addCreditBtn;
+    public Button removeCreditBtn;
+    public Button editProductionIDBtn;
+    public Button saveBtn;
+    public TextField nameField;
+    public TextField roleField;
+    public TextField emailField;
+    public TextField addressField;
+    public TextField phoneField;
+    public ToggleGroup rGroup1;
+    public RadioButton castRadio;
+    public RadioButton audioRadio;
+    public RadioButton visualRadio;
     @FXML
     private Button backButton;
 
@@ -38,5 +50,56 @@ public class CreditsController {
         audioLV.setItems(audioList);
         visualList = FXCollections.observableArrayList();
         visualLV.setItems(visualList);
+    }
+
+    public void btnHandler(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == addCreditBtn) {
+            System.out.println("hej hans");
+            String name = nameField.getText();
+            String role = roleField.getText();
+            String address = addressField.getText();
+            String phone = phoneField.getText();
+            String email = emailField.getText();
+            Person person = new Person(name, role, address, phone, email);
+            if (rGroup1.getSelectedToggle() == castRadio) {
+                castList.add(person);
+            } else if (rGroup1.getSelectedToggle() == audioRadio) {
+                audioList.add(person);
+            } else if (rGroup1.getSelectedToggle() == visualRadio) {
+                visualList.add(person);
+        }}
+
+            if (actionEvent.getSource() == removeCreditBtn) {
+                System.out.println("daa");
+
+                Person selectedPerson = (Person) audioLV.getSelectionModel().getSelectedItem();
+                if (selectedPerson != null){
+                    audioList.remove(selectedPerson);
+                }
+                else {
+                    selectedPerson = (Person) castLV.getSelectionModel().getSelectedItem();
+                }
+
+                if (selectedPerson != null){
+                    castList.remove(selectedPerson);
+                }
+                else {
+                    selectedPerson = (Person) visualLV.getSelectionModel().getSelectedItem();
+                }
+
+                if (selectedPerson != null){
+                    visualList.remove(selectedPerson);
+                }
+                else {
+                    System.out.println("wut?");
+                }
+            }
+        if (actionEvent.getSource() == editProductionIDBtn) {
+            System.out.println("edit");
+        }
+        if (actionEvent.getSource() == saveBtn) {
+            System.out.println("save");
+        }
+
     }
 }
